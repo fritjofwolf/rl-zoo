0fcc754439e17c38b0fe234019ee948cac0d1636
Fritjof Wolf <fritjofwolf@gmail.com>
2019-01-17 02:16:35 +0100

    small hotfixes

--- src/visualization.py
+++ src/visualization.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-%matplotlib inline
+import numpy as np
 
 def visualize_episode_return(episode_returns, smoothing_window = 100, title=''):
     n_runs = len(episode_returns)
@@ -9,5 +9,5 @@ def visualize_episode_return(episode_returns, smoothing_window = 100, title=''):
     for i in range(n_runs):
         plt.plot(range(smoothing_window,len(episode_mean_returns[i])+smoothing_window), episode_mean_returns[i], color=np.random.rand(3))
     plt.xlabel('Episodes')
-    plt.ylabel('Mean Episode Return (smoothed over time window of' + smoothing_window + 'steps)')
+    plt.ylabel('Mean Episode Return (smoothed over time window of' + str(smoothing_window) + 'steps)')
     plt.title(title)
