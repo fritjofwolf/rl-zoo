f0ca2b37185f64aa01a52f2fbdf5dc6056222443
Fritjof Wolf <fritjofwolf@gmail.com>
2019-03-08 02:24:12 +0100

    Clean up repo

--- src/test_file.py
+++
@@ -1,9 +0,0 @@
-from dqn import DQN
-import numpy as np
-import tkinter
-import matplotlib.pyplot as plt
-
-dqn_model = DQN('CartPole-v0')
-results = dqn_model.train(1000)
-print(results)
-plt.plot([x[0] for x in results], [x[1] for x in results])
