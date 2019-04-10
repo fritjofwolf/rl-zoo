08c00ca89cd924036166b26cf1e01a53bf1328c0
Fritjof Wolf <fritjofwolf@gmail.com>
2019-04-10 14:48:01 +0200

    Added method for saving and loading a pretrained model; currently broken

--- openai_spinning_up/src/algorithms/ppo.py
+++ openai_spinning_up/src/algorithms/ppo.py
@@ -1,19 +1,23 @@
 import tensorflow as tf
 import tensorflow_probability as tfp
 import gym
+import json
 import numpy as np
 import logging
 from tqdm import tqdm_notebook
+# from tensorflow.keras.models import load_model
+from tensorflow.keras.models import model_from_json
 
 from ..data_collection.a2c_data_collection import A2CDataCollector
 
 class PPO():
 
-    def __init__(self, env_name, gamma = 0.9, learning_rate = 0.0003):
+    def __init__(self, env_name, gamma = 0.9, learning_rate = 0.0003, model_path = None):
         self._env = gym.make(env_name)
         self._learning_rate = learning_rate
         self._env_name = env_name
         self._gamma = gamma
+        self._model_path = model_path
         # self._obs_dim = 100800
         self._obs_dim = self._env.observation_space.shape[0]
         if type(self._env.action_space) == gym.spaces.box.Box:
@@ -27,7 +31,7 @@ class PPO():
 
 
     def train(self, n_epochs, K = 5):
-        [obs_ph, act_ph, new_obs_ph, rew_ph, terminal_ph, policy_network, old_policy_network, actions, train_policy, train_state_value] = self._graph
+        [obs_ph, act_ph, new_obs_ph, rew_ph, terminal_ph, policy_network, old_policy_network, state_value_network, actions, train_policy, train_state_value] = self._graph
         data_collector = A2CDataCollector(self._sess, self._env_name, actions, obs_ph, 20, 20)
         for i in tqdm_notebook(range(n_epochs)):
             self._update_old_network()
@@ -48,7 +52,19 @@ class PPO():
                                             rew_ph: np.array(rews).reshape(-1, 1),
                                             terminal_ph: np.array(terminal).reshape(-1, 1)
                                         })
+        self._save_model()
         return data_collector.get_episode_statistics()
+
+    def _save_model(self):
+        checkpoint_path = '/home/janus/models/'
+        # serialize model to JSON
+        model_json = self._graph[5].to_json()
+        with open(checkpoint_path + "policy_model.json", "w") as json_file:
+            json_file.write(model_json)
+        # serialize weights to HDF5
+        self._graph[5].save_weights(checkpoint_path+'policy_network.h5')
+        # self._graph[5].save(checkpoint_path+'policy_network.h5')
+        # self._graph[7].save(checkpoint_path+'state_network.h5')
 
     def _update_old_network(self):
         policy_network = self._graph[5]
@@ -93,9 +109,22 @@ class PPO():
         train_state_value = state_value_optimizer.minimize(state_value_loss)
         self._sess = tf.Session()
         self._sess.run(tf.global_variables_initializer())
+        if self._model_path:
+            # load json and create model
+            json_file = open(self._model_path+'policy_model.json', 'r')
+            loaded_model_json = json_file.read()
+            json_file.close()
+            policy_network = model_from_json(loaded_model_json)
+            # load weights into new model
+            policy_network.load_weights(self._model_path+'policy_network.h5')
+            print("Loaded model from disk")
+        #     policy_network = load_model(self._model_path+'/policy_network.h5')
+        #     old_policy_network = load_model(self._model_path+'/policy_network.h5')
+        #     state_value_network = load_model(self._model_path+'/state_network.h5')
+        #     print('Model loaded successfully')
 
         self._graph = [obs_ph, act_ph, new_obs_ph, rew_ph, terminal_ph, \
-                        policy_network, old_policy_network, actions, train_policy, train_state_value]
+                        policy_network, old_policy_network, state_value_network, actions, train_policy, train_state_value]
 
     def _build_computational_graph_continuous_actions(self):
         # define placeholder
